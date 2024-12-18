f3cfdb967fb462dd8556e93557e4082ff84e065d
hxm <125423559@qq.com>
2024-12-19 01:21:53 +0800

    submodule commit

---
+++ clients/apple/SFT/ApplicationDelegate.swift
@@ -0,0 +1,24 @@
+import ApplicationLibrary
+import Foundation
+import Libbox
+import Library
+import UIKit
+
+class ApplicationDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        NSLog("Here I stand")
+        LibboxSetup(FilePath.sharedDirectory.relativePath, FilePath.workingDirectory.relativePath, FilePath.cacheDirectory.relativePath, true)
+        LibboxSetLocale(Locale.current.identifier)
+        setup()
+        return true
+    }
+
+    private func setup() {
+        do {
+            try UIProfileUpdateTask.configure()
+            NSLog("setup background task success")
+        } catch {
+            NSLog("setup background task error: \(error.localizedDescription)")
+        }
+    }
+}
